c300cac2fe80e0357f53e78ed42acacf99a9c778
Pat31477 <pat31477@yahoo.com>
2021-04-16 17:10:16 -0400

    first commit of tests folder and intern.test.js file

---
+++ tests/Intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require("../lib/Intern");
+
+test("Set school via constructor", () => {
+  const testValue = "SJU";
+  const e = new Intern("Bug", 1, "email@email.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const e = new Intern("Bug", 1, "email@email.com", "UCLA");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("CGet school via getSchool()", () => {
+  const testValue = "SJU";
+  const e = new Intern("Bug", 1, "email@email.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
